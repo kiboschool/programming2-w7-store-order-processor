71d0527429a46950e2fd0d7f11bbefb1e9f8c7dd
moltenform.com(Ben Fisher) <amoltenform@gmail.com>
2023-02-21 00:35:30 -0800

    add solution

--- main.py
+++ main.py
@@ -1,32 +1,94 @@
+
 import json
 
 types = ['jacket', 'slacks', 'pair_of_shoes']
 brands = ['fruche', 'onalaja', 'kente']
 starting_value = 20
 
+
 class StoreOrderProcessor:
     def __init__(self):
-        # please write your intitialization code here.
-        pass
-    
+        self.inventory = {}
+        for type in types:
+            for brand in brands:
+                key = (type, brand)
+                self.inventory[key] = starting_value
+
+        self.is_valid = True
+
+    def process_list(self, list_of_items):
+        for item in list_of_items:
+            self.process_one_item(item)
+
+        self.check_has_full_outfit()
+        return self.get_result_string()
+
     def process(self, filename):
         with open(filename, 'r') as file:
-            list_of_items = json.load(file)
-            
+            try:
+                list_of_items = json.load(file)
+            except:
+                self.is_valid = False
+                list_of_items = []
+
         results = self.process_list(list_of_items)
         print(results)
-        
-    def process_list(self, list_of_items):
-        # please write your processing code here,
-        # it should loop through list_of_items and process each item.
-        # helper methods are fine, and recommended.
-        
-        results = 'Remaining inventory:'
-        # then, add the inventory text to the results string.
-        
+
+    def process_one_item(self, item):
+        type = item.get('type')
+        brand = item.get('brand')
+        quantity = item.get('quantity')
+
+        if item.get('type') not in types:
+            self.is_valid = False
+            return
+
+        if item.get('brand') not in brands:
+            self.is_valid = False
+            return
+
+        try:
+            quantity = int(quantity)
+        except ValueError:
+            self.is_valid = False
+            return
+
+        key = (type, brand)
+        self.inventory[key] -= quantity
+        if self.inventory[key] < 0:
+            self.is_valid = False
+            return
+
+    def check_has_full_outfit(self):
+        self.has_full_outfit = False
+        for brand in brands:
+            is_full_for_this_brand_so_far = True
+            for type in types:
+                key = (type, brand)
+                if self.inventory[key] == starting_value:
+                    is_full_for_this_brand_so_far = False
+
+            if is_full_for_this_brand_so_far:
+                self.has_full_outfit = True
+
+    def get_result_string(self):
+        if not self.is_valid:
+            return 'Invalid input'
+
+        results = 'Remaining inventory:\n'
+        for type in types:
+            for brand in brands:
+                key = (type, brand)
+                results += f'{type} {brand} {self.inventory[key]}\n'
+
+        if self.has_full_outfit:
+            results += 'Contains full outfit for a brand\n'
+
         return results
-    
+
+
+
 if __name__ == '__main__':
     processor = StoreOrderProcessor()
-    processor.process('example1.json')
+    processor.process('example3.json')
     
--- test_store_order_processor.py
+++ test_store_order_processor.py
@@ -1,20 +1,181 @@
+
 import unittest
 import main
 
 
 class TestStoreOrderProcessor(unittest.TestCase):
-    def test_example(self):
+    def test_crashes_on_invalid_type(self):
         processor = main.StoreOrderProcessor()
-        # ... add a test here
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slcks(typo)", "brand": "kente", "quantity": "1"}
+])
+        assert results.strip() == 'Invalid input'
+        
+    def test_crashes_on_invalid_brand(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kent-typo", "quantity": "1"}
+])
+        assert results.strip() == 'Invalid input'
+        
+    def test_crashes_on_empty_string_or_None_quantity(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": ""}
+])
+        assert results.strip() == 'Invalid input'
+        
+    def test_crashes_on_alphabetic_quantity(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "abc"}
+])
+        assert results.strip() == 'Invalid input'
+        
+    def test_still_valid_if_exceeds_inventory_across_multiple_rows(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "18"},
+    {"type": "slacks", "brand": "kente", "quantity": "4"}
+])
+        assert results.strip() == 'Invalid input'
+        
+    def test_off_by_one_error(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "20"},
+])
+        assertEqualStrModuloNewlines(results, '''Remaining inventory:
+jacket fruche 18
+jacket onalaja 20
+jacket kente 20
+slacks fruche 20
+slacks onalaja 20
+slacks kente 0
+pair_of_shoes fruche 20
+pair_of_shoes onalaja 20
+pair_of_shoes kente 20''')
     
-    # todo: please add many tests here!
-    # The tests can call into either the main process_list() method,
-    #  or a helper method you have added.
+    def test_still_valid_if_exceeds_inventory(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "22"},
+])
+        assert results.strip() == 'Invalid input'
+        
+    def test_says_full_outfit_regardless_of_brand(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "fruche", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "2"},
+    {"type": "pair_of_shoes", "brand": "kente", "quantity": "2"},
+])
+        assertEqualStrModuloNewlines(results, '''Remaining inventory:
+jacket fruche 18
+jacket onalaja 20
+jacket kente 20
+slacks fruche 20
+slacks onalaja 20
+slacks kente 18
+pair_of_shoes fruche 20
+pair_of_shoes onalaja 20
+pair_of_shoes kente 18''')
+
+    def test_says_full_outfit_if_there_are_2_slacks_and_1_jacket(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "kente", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "2"},
+])
+        assertEqualStrModuloNewlines(results, '''Remaining inventory:
+jacket fruche 20
+jacket onalaja 20
+jacket kente 18
+slacks fruche 20
+slacks onalaja 20
+slacks kente 16
+pair_of_shoes fruche 20
+pair_of_shoes onalaja 20
+pair_of_shoes kente 20''')
+
+    def test_says_full_outfit_even_if_one_of_the_quantities_is_0(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "kente", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "2"},
+    {"type": "pair_of_shoes", "brand": "kente", "quantity": "0"},
+])
+        assertEqualStrModuloNewlines(results, '''Remaining inventory:
+jacket fruche 20
+jacket onalaja 20
+jacket kente 18
+slacks fruche 20
+slacks onalaja 20
+slacks kente 18
+pair_of_shoes fruche 20
+pair_of_shoes onalaja 20
+pair_of_shoes kente 20''')
+
+    def test_has_a_complete_outfit_(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "onalaja", "quantity": "2"},
+    {"type": "slacks", "brand": "onalaja", "quantity": "2"},
+    {"type": "pair_of_shoes", "brand": "onalaja", "quantity": "1"},
+])
+        assertEqualStrModuloNewlines(results, '''Remaining inventory:
+jacket fruche 20
+jacket onalaja 18
+jacket kente 20
+slacks fruche 20
+slacks onalaja 18
+slacks kente 20
+pair_of_shoes fruche 20
+pair_of_shoes onalaja 19
+pair_of_shoes kente 20
+Contains full outfit for a brand''')
+
+    def test_crashes_if_given_2_complete_outfits_(self):
+        processor = main.StoreOrderProcessor()
+        results = processor.process_list([
+    {"type": "jacket", "brand": "kente", "quantity": "2"},
+    {"type": "slacks", "brand": "kente", "quantity": "2"},
+    {"type": "pair_of_shoes", "brand": "kente", "quantity": "1"},
+    {"type": "jacket", "brand": "onalaja", "quantity": "2"},
+    {"type": "slacks", "brand": "onalaja", "quantity": "2"},
+    {"type": "pair_of_shoes", "brand": "onalaja", "quantity": "1"},
+])
+        assertEqualStrModuloNewlines(results, '''Remaining inventory:
+jacket fruche 20
+jacket onalaja 18
+jacket kente 18
+slacks fruche 20
+slacks onalaja 18
+slacks kente 18
+pair_of_shoes fruche 20
+pair_of_shoes onalaja 19
+pair_of_shoes kente 19
+Contains full outfit for a brand''')
+
+
+def assertEqualStrModuloNewlines(s1, s2):
+    s1 = s1.replace('\r\n', '\n').strip()
+    s2 = s2.replace('\r\n', '\n').strip()
+    if False:
+        if s1 != s2:
+            print(s1+'---')
+            print(s2+'---')
     
-    # You should check for all features,
-    # including cases where the order would run out of inventory,
-    # including cases where there is a complete outfit for a brand,
-    # and checking that invalid input will cause the output to be 'Invalid input'
+    assert s1==s2
+
 
 
 if __name__ == "__main__":
